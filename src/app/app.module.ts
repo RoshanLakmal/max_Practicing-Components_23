d12278ca2630dae54b564ecb1c74fe94459a513e
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-20 23:44:31 +1100

    created warning and success components and added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warningalert',
+    templateUrl: './warningalert.component.html',
+    styles: [`
+        .warning {
+            color: red;
+        }
+    `]
+})
+export class WarningAlertComponent {
+
+}
